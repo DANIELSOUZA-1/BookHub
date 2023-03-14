35c431fcf366fe979f7f34c07672451062a2096c
Daniel Antônio <66747998+DANIELSOUZA-1@users.noreply.github.com>
2023-03-14 20:58:24 -0300

    details page

--- list/list.js
+++ list/list.js
@@ -1,12 +1,10 @@
-/* Receitas listener session */
+/* Books add button listener session */
 function booksStartListener(button) {
     button.addEventListener("mouseover", () => {
-        debugger
         label = document.getElementById('books-label-button')
         label.style.display = 'inline'
     })
     button.addEventListener("mouseout", () => {
-        debugger
         label = document.getElementById('books-label-button')
         label.style.display = 'none'
     })
